c15f9078071e16b622be1a67c9ae104811c5f392
Andrey <Ivana_2004@mail.ru>
2020-04-28 20:59:24 +0300

    first commit

---
+++ fp-1.cpp
@@ -0,0 +1,84 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Task - find sum on first n elements in array
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+// compiler options
+// -Wall -std=c++14 -O2 -o a.out source_file.cpp
+// -Wall -std=c++14   -o a.out source_file.cpp
+
+#include <iostream>
+
+typedef unsigned long long int ull;
+
+const int N = 1E8;
+int m[N];
+
+
+
+// imperative mutable algorythm
+ull f(int n) {
+    ull s = 0;
+    for (int i=0; i<=n; i++) s += m[i];
+    return s;
+}   
+
+
+
+// non-tail recursion immutable algorythm
+ull g(int n) { return (n<0) ? 0 : m[n] + g(n-1); }
+
+
+
+// tail recursion immutable algorythm
+ull h(int n, ull s) { return (n<0) ? s : h(n-1, m[n] + s); }
+
+
+
+// binary-split recursion immutable algorythm
+ull d(int b, int e) {
+    int c = (b + e)/2;
+    return (b==e) ? m[b] : d(b, c) + d(c+1, e);
+}
+
+
+
+int main() {
+    for (int i=0; i<N; i++) m[i] = i;
+    
+    int n = 1E1; //1E7;
+    std::cout << f(n) << "\n";
+    std::cout << g(n) << "\n";
+    std::cout << h(n, 0) << "\n";
+    std::cout << d(0, n) << "\n";
+}
+
+
+
+// Max recursion depth calculation. Can you do it in functional way? :)
+/*
+int max_l = 0;
+
+ull d(int b, int e, int l) {
+    int c = (b + e)/2;
+    int nxt_l = l + 1;
+    max_l = std::max(max_l, nxt_l);
+    
+    return (b==e) ? m[b] : d(b, c, nxt_l) + d(c+1, e, nxt_l);
+}
+
+    max_l = 0;
+    std::cout << d(0, n, 0) << "\n";
+    std::cout << max_l << "\n";
+*/
+
+
+
+// Haskel code
+/*
+m i = i
+
+h n s = if (n<0) then s else h (n-1) (m n + s)
+
+main = print $ h 10 0
+*/
+
---
+++ fp-2.cpp
@@ -0,0 +1,80 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Task - check existed numbers of range [0..10] in array
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int N = 1E3, M = 11;
+int m[N];
+
+
+
+// imperative mutable algorythm
+
+bool r[M];
+
+void proc_iter(int n) { for (int i=0; i<n; i++) r[m[i]] = 1; }
+
+void show_iter() {
+    for (int i=0; i<M; i++)
+        cout << i << "\t" << (r[i] ? "YES" : "-") << "\n";
+}
+
+
+
+// immutable data structure with interface
+
+typedef int acc_t;
+
+acc_t empty_acc = 0;
+
+acc_t set(acc_t a, int i) { return a | (1 << i); }
+
+bool  get(acc_t a, int i) { return a & (1 << i); }
+
+
+
+// tail recursion immutable algorythm
+
+acc_t f(int n, acc_t a) { return (n<0) ? a : f(n-1, set(a, m[n])); }
+
+string row(acc_t a, int i) {
+    return to_string(i) + "\t" + (get(a, i) ? "YES" : "-");
+}
+
+string st(acc_t a, int i, string s) {
+    string s_new = s + "\n" + row(a, i);
+    return (i>=M) ? s : st(a, i+1, s_new);
+}
+
+
+
+// non-tail recursion immutable algorythm
+
+acc_t g(int n) { return (n<0) ? empty_acc : set(g(n-1), m[n]); }
+
+string sr(acc_t a, int i) {
+    string s = "\n" + row(a, i);
+    return (i>=M) ? "" : s + sr(a, i+1);
+}
+
+
+int main() {
+    int x, n = 0; while (cin >> x) m[n++] = x;
+    
+    proc_iter(n);
+    show_iter();
+    
+    acc_t a0 = f(n-1, empty_acc);
+    cout << st(a0, 0, "") << "\n";
+    
+    acc_t a1 = g(n-1);
+    cout << sr(a1, 0) << "\n";
+}
+
+// input
+// 2 3 3 3 3 3 3 2 2 2 7 3 3 1
+
